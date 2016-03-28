0ef10863da72d14b3566cad599a44f16f313106f
fanxiangyang <fanxiangyang_heda@163.com>
2016-03-28 09:47:28 +0800

    Initial Commit

---
+++ FanCollectionViewFlowlayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanCollectionViewFlowlayout
+//
+//  Created by 向阳凡 on 16/3/28.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FanCollectionViewFlowlayoutTests/FanCollectionViewFlowlayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  FanCollectionViewFlowlayoutTests.swift
+//  FanCollectionViewFlowlayoutTests
+//
+//  Created by 向阳凡 on 16/3/28.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+import XCTest
+@testable import FanCollectionViewFlowlayout
+
+class FanCollectionViewFlowlayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
